1255b19fed28c9553f37c77d85cdf39545722919
Julian Connor <connoj3@rpi.edu>
2010-11-14 21:20:13 -0500

    Most things are working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,14 @@ class HomeController < ApplicationController
     @items = Category.where(:parent => "Items")
     @jobs = Category.where(:parent => "Jobs")
     @rent = Category.where(:parent => "Rent")
-    @latest_listings = Listing.order("created_at DESC").limit(4)
-    @latest_housings = Housing.order("created_at DESC").limit(4)
+    @latest_listings = Listing.order("created_at DESC").limit(3)
+    @latest_housings = Housing.order("created_at DESC").limit(3)
   end
   
   def search
     # Build hash of results
-    c_id = params[:category_id].to_i
-    if c_id == 0
+    c_id = params[:category_id]
+    if c_id == "all" # User wants to search all models
       @h_results = Housing.search() do
         keywords(params[:searchbar])
       end
@@ -19,7 +19,16 @@ class HomeController < ApplicationController
         keywords(params[:searchbar])
       end
       # Job results go here
+    elsif c_id == "listings"
+      @l_results = Listing.search() do
+        keywords(params[:searchbar])
+      end
+    elsif c_id == "housings"
+       @h_results = Housing.search() do
+          keywords(params[:searchbar])
+        end
     else
+      c_id = c_id.to_i
       if c_id > 0 and c_id <= 6 # Category ID points at listing items
         @l_results = Listing.search() do
           keywords(params[:searchbar])
@@ -30,7 +39,7 @@ class HomeController < ApplicationController
       else # Category ID points to housings
         @h_results = Housing.search() do
           keywords(params[:searchbar])
-          with(:category_id.equal_to(params[:category_id])
+          with(:category_id).equal_to(params[:category_id])
         end
       end
     end
