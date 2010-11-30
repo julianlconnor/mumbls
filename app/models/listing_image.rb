50e10707e353191f9e1bb6986614e981484f1394
Julian Connor <connoj3@rpi.edu>
2010-11-29 20:40:38 -0500

    Added category count functionality and fixed email registration

--- app/models/listing_image.rb
+++ app/models/listing_image.rb
@@ -2,7 +2,7 @@ class ListingImage < ActiveRecord::Base
   
   belongs_to :listing
   
-  has_attached_file :photo, :styles => { :thumb => "50x50", :small => "140x140#", :large => "550x456>" }
+  has_attached_file :photo, :styles => { :thumb => "50x50#", :small => "140x140#", :large => "550x456>" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 2.megabytes
 
