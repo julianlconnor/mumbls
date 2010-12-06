ea2020b8b7ee0047ad2222944e85c9e3c7fba3a1
Julian Connor <connoj3@rpi.edu>
2010-12-06 18:08:49 -0500

    Fixing s3 path once again.

--- app/models/listing_image.rb
+++ app/models/listing_image.rb
@@ -5,7 +5,7 @@ class ListingImage < ActiveRecord::Base
   has_attached_file :photo,
                     :storage => :s3,
                     :s3_credentials =>"#{RAILS_ROOT}/config/s3.yml",
-                    :path => "/listings/:listing/:style/:filename",
+                    :path => "/listings/:id/:style/:filename",
                     :styles => { :thumb => "50x50#", :small => "140x140#", :large => "550x456>" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 2.megabytes
