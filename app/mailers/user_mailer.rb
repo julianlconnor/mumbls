20c29408eeb23246969c0ffb0e4fb32d09e199b0
Raphael Eidus <raphaeleidus@gmail.com>
2010-12-05 22:47:55 -0500

    more url fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
   
   def activation_email(user)
     @user = user
-    @url  = "http://www.mumbls.com/activate/#{@user.activation_code}"
+    @url  = "http://mumbls.heroku.com/activate/#{@user.activation_code}"
     email_with_name = "#{@user.name} <#{@user.email}>"
     mail(:to => user.email, :subject => "Welcome to Mumbls.com")
   end
