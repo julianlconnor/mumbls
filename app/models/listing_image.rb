88b45f60d395dfa95abe7c74b5b823051163a538
Julian Connor <connoj3@rpi.edu>
2010-12-06 13:16:50 -0500

    Added amazon s3 & gmap key helper function

--- app/models/listing_image.rb
+++ app/models/listing_image.rb
@@ -2,7 +2,10 @@ class ListingImage < ActiveRecord::Base
   
   belongs_to :listing
   
-  has_attached_file :photo, :styles => { :thumb => "50x50#", :small => "140x140#", :large => "550x456>" }
+  has_attached_file :photo,
+                    :storage => :s3,
+                    :s3_credentials =>"#{RAILS_ROOT}/config/s3.yml",
+                    :styles => { :thumb => "50x50#", :small => "140x140#", :large => "550x456>" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 2.megabytes
 
