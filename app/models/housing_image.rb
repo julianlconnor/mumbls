83a9d85a69d7b81962aaa14864b1440065aed62f
Julian Connor <connoj3@rpi.edu>
2010-12-06 17:28:16 -0500

    S3 path fix

--- app/models/housing_image.rb
+++ app/models/housing_image.rb
@@ -4,6 +4,7 @@ class HousingImage < ActiveRecord::Base
   has_attached_file :photo,
                     :storage => :s3,
                     :s3_credentials =>"#{RAILS_ROOT}/config/s3.yml",
+                    :path => "/housings/:style/:filename",
                     :styles => { :small => "150x150#", :large => "320x240>" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 3.megabytes
