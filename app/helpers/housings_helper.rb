d920f5d46a3cb1a58294b2f2d060e16eff875fcd
Julian Connor <connoj3@rpi.edu>
2010-12-06 17:53:45 -0500

    Trying to fix google maps again

--- app/helpers/housings_helper.rb
+++ app/helpers/housings_helper.rb
@@ -1,31 +1,32 @@
 module HousingsHelper
   def show_housing_listing(address)
     "$(function() { 
-        $('#map').gMap({ markers: [{ address: '#{address}',
-                                      html: '#{address}' }],
-                          address: '#{address}',
-                          zoom: 15 }); 
-    });"
+                        $('#map').gMap({ markers: [{ address: '#{address}',
+                                                      html: '#{address}' }],
+                                          address: '#{address}',
+                                          zoom: 15 }); 
+                    });"
   end
+  
   def show_all(housings)
-    if housings.count > 0
+    if !housings.nil? and housings.count > 0
       marker_str = "markers: ["
       housings.each do |housing|
         truncated_description = truncate(housing.description,:length => 45)
         marker_str = marker_str + "{ address: '#{housing.address},#{housing.city},#{housing.state},#{housing.zip_code}',
-                                     html: '<b>#{housing.title} - $#{housing.price} ~ #{truncated_description}</b>'
-                                   },"
+                           html: '<b>#{housing.title} - $#{housing.price} ~ #{truncated_description}</b>'
+                         },"
       end
       marker_str = marker_str.chop
       marker_str = marker_str + "]"
-      "$(function() { 
-          $('#map').gMap({#{marker_str}, zoom: 15 }); });"
+      "$(function() { $('#map').gMap({#{marker_str}, zoom: 15 }); });"
     else
       "$(function() { 
-          $('#map').gMap({ 
-                            address: '15th St & RPI Walk over Bridge',
-                            zoom: 15 }); 
-      });"
+                          $('#map').gMap({ address: '15th St & RPI Walk over Bridge',
+                                            zoom: 15 }); 
+                      });"
     end
   end
+  
+  
 end
