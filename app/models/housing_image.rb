1fdddba5d19a6bd113c2aeb4502d02b80f88739c
Julian Connor <connoj3@rpi.edu>
2010-12-08 17:28:10 -0500

    Changed housing_images small size from 150x150 to 140x140.

--- app/models/housing_image.rb
+++ app/models/housing_image.rb
@@ -5,7 +5,7 @@ class HousingImage < ActiveRecord::Base
                     :storage => :s3,
                     :s3_credentials =>"#{RAILS_ROOT}/config/s3.yml",
                     :path => "/housings/:id/:style/:filename",
-                    :styles => { :small => "150x150#", :large => "320x240>" }
+                    :styles => { :small => "140x140#", :large => "320x240>" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 3.megabytes
 
